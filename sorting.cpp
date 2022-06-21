bac1f32a0b9b9ae97e89b8c073294e11ccfd0eeb
Diego Palma <dpalmasan@gmail.com>
2022-06-21 00:06:14 -0700

    Add bubble and merge sort (#11)

---
+++ sorting.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+
+void bubble_sort(int* arr, const std::size_t& n) {
+    for (std::size_t i = 0; i < n; ++i) {
+        for (std::size_t j = 0; j < n - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void __merge(int* arr, const std::size_t& a, const std::size_t& b, const std::size_t& mid) {
+    auto *tmp1 = new int[mid - a + 1], *tmp2 = new int[b - mid];
+    for (auto i = 0; i < mid - a + 1; ++i) {
+        tmp1[i] = arr[a + i];
+    }
+    for (auto i = 0; i < b - mid; ++i) {
+        tmp2[i] = arr[mid + 1 + i];
+    }
+
+    std::size_t i = 0, j = 0, k = a;
+
+    while (i < mid - a + 1 && j < b - mid) {
+        if (tmp1[i] < tmp2[j]) {
+            arr[k++] = tmp1[i++];
+        } else {
+            arr[k++] = tmp2[j++];
+        }
+    }
+    while (i < mid - a + 1) {
+        arr[k++] = tmp1[i++];
+    }
+    while (j < b - mid) {
+        arr[k++] = tmp2[j++];
+    }
+}
+
+void __sort(int* arr, const std::size_t& a, const std::size_t& b) {
+    if (a >= b) {
+        return;
+    }
+
+    std::size_t mid = a + (b - a) / 2;
+    __sort(arr, a, mid);
+    __sort(arr, mid + 1, b);
+    __merge(arr, a, b, mid);
+}
+
+void merge_sort(int* arr, const std::size_t& n) {
+    __sort(arr, 0, n - 1);
+}
---
+++ sorting_test.cpp
@@ -0,0 +1,45 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include "sorting.h"
+
+
+bool is_sorted(int* arr, std::size_t n) {
+    for (std::size_t i = 0; i < n - 1; ++i) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+TEST_CASE("Test bubble sort") {
+    int arr[8] = {1, 3, 8, 2, 9, 2, 5, 6};
+    bubble_sort(arr, 8);
+    CHECK(is_sorted(arr, 8));
+}
+
+TEST_CASE("Test merge sort") {
+    SUBCASE("Merge intertwined arrays") {
+        int arr[6] = {1, 3, 5, 2, 4, 6};
+        __merge(arr, 0, 5, 5 / 2);
+        CHECK(is_sorted(arr, 6));
+    }
+
+    SUBCASE("Merge all left") {
+        int arr[6] = {1, 2, 3, -3, -2, -1};
+        __merge(arr, 0, 5, 5 / 2);
+        CHECK(is_sorted(arr, 6));
+    }
+
+    SUBCASE("Merge all right") {
+        int arr[6] = {-3, -2, -1, 1, 2, 3};
+        __merge(arr, 0, 5, 5 / 2);
+        CHECK(is_sorted(arr, 6));
+    }
+
+    SUBCASE("Sort array") {
+        int arr[8] = {1, 3, 8, 2, 9, 2, 5, 6};
+        merge_sort(arr, 8);
+        CHECK(is_sorted(arr, 8));
+    }
+}
